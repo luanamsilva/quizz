6ddb1ac4a4001164eca194515bf0e7e392d1d0a8
luanamsilva <luanamoura2002@yahoo.com.br>
2023-07-03 11:10:34 -0300

    structuring-quizz

---
+++ src/app/data/questions.ts
@@ -0,0 +1,16 @@
+import { Question } from "../types/Question";
+
+export const questions:Question[] = [{
+question: "Como se define um evento de clique em um elemento HTML usando JavaScript?",
+  options: [
+    ' Atribuindo uma função ao evento "onclick" do elemento desejado',
+    ' Atribuindo uma função ao evento "onChange" do elemento desejado',
+    ' Atribuindo uma função ao evento "click" do elemento desejado',
+   ' Utilizando a tag "<script>" com o atributo apontando para onde a página vai levar.'
+
+  ],
+  answer: 0
+
+}
+  
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+"use client"
+import { useState } from "react";
+import { questions } from "./data/questions";
+
 
 export default function Home() {
+  const [currentQuestion, setCurrentQuestion] = useState(0)
   return (
-    <main>
-      teste
-    </main>
+<div className="w-full h-screen flex justify-center items-center text-gray-300">
+<div className="w-full max-w-xl rounded-md bg-zinc-800 shadow shadow-zinc-900">
+<h1 className="p-5 text-center">Quizz JavaScript</h1>
+<div className="p-5">Perguntas</div>
+<div className="p-5 ">Respostas</div>
+<div className="text-center">{currentQuestion+1} de {questions.length} pergunta{questions.length === 1? '': 's'} </div>
+</div>
+</div>
   )
 }
