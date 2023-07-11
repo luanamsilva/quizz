d4634f9296f57549f318c8da1e6e4acb036b7136
luanamsilva <luanamoura2002@yahoo.com.br>
2023-07-11 09:31:57 -0300

    showResults

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { questions } from "./data/questions";
 import { QuestionItem } from "./components/QuestionItem";
-
+import {Results} from './components/Results'
 
 
 
@@ -10,10 +10,13 @@ export default function Home() {
 
   const [currentQuestion, setCurrentQuestion] = useState(0)
 const [answers, setAnswers] = useState<number[]>([])
+const [showResults, setShowResults] = useState(false)
 
 const nextQuestion = ()=>{
 if(questions[currentQuestion+1]){
   setCurrentQuestion(currentQuestion +1)
+} else{
+  setShowResults(true)
 }
 }
 
@@ -26,9 +29,18 @@ nextQuestion()
 <div className="w-full max-w-xl rounded-md bg-cyan-950shadow shadow-zinc-900">
 <h1 className="p-5 text-center text-bold text-cyan-500 text-xl">Quizz JavaScript</h1>
 <div className="p-5">
- <QuestionItem question={questions[currentQuestion]} onAnswer={handlerAnswer} count={currentQuestion+1}/>
+  {!showResults &&
+ <QuestionItem question={questions[currentQuestion]} onAnswer={handlerAnswer} count={currentQuestion+1}/>}
+{showResults &&
+<Results questions={questions} answers={answers}/>
+}
+
 </div>
-<div className="text-center">{currentQuestion+1} de {questions.length} pergunta{questions.length === 1? '': 's'} </div>
+<div className="text-center">
+  {!showResults && `${currentQuestion+1} de ${questions.length} pergunta${questions.length === 1? '':'s'}` }
+  {showResults && 
+  <button className="px-3 py-2 rounded-md bg-blue-800">Reininciar</button>}
+  </div>
 </div>
 </div>
   )
